b89abd83c6374e482aff6af78b21c6b4ab9849ab
wclcatdog <77136368+wclcatdog@users.noreply.github.com>
2023-12-14 21:59:48 +0800

    Add files via upload

--- transformer_module.py
+++ transformer_module.py
@@ -1,4 +1,91 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import math
+
 class PositionalEncoder(nn.Module):
-  
+    def __init__(self,d_model,max_seq,max_seq_len = 80):
+        super().__init__()
+        self.d_model = d_model
+
+        pe = torch.zeros(max_seq_len,d_model)
+        for pos in range(max_seq_len):
+            for i in range(0, d_model, 2):
+                pe[pos, i] = math.sin(pos / (10000 ** ((2 * i) / d_model)))
+                pe[pos, i + 1] = math.cos(pos / (10000 ** ((2 * (i + 1)) / d_model)))
+
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+
+        x = x * math.sqrt(self.d_model)
+
+        seq_len = x.size(1)
+        x = x + Variable(self.pe[:, :seq_len], requires_grad=False).cuda()
+
+        return x
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, heads, d_model, dropout = 0.1):
+        super().__init__()
+        self.d_model = d_model
+        self.d_k = d_model // heads
+        self.h = heads
+        self.q_linear = nn.Linear(d_model, d_model)
+        self.v_linear = nn.Linear(d_model, d_model)
+        self.k_linear = nn.Linear(d_model, d_model)
+        self.dropout = nn.Dropout(dropout)
+        self.out = nn.Linear(d_model, d_model)
+
+
+    def attention(q, k, v, d_k, mask=None, dropout=None):
+        scores = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(d_k)
+        # 掩盖掉那些为了填补长度增加的单元，使其通过 softmax 计算后为 0
+        if mask is not None:
+            mask = mask.unsqueeze(1)
+            scores = scores.masked_fill(mask == 0, -1e9)
+
+        scores = F.softmax(scores, dim=-1)
+
+        if dropout is not None:
+            scores = dropout(scores)
+
+        output = torch.matmul(scores, v)
+
+        return output
+
+    def forward(self, q, k, v, mask=None):
+        bs = q.size(0)
+        # 进行线性操作划分为成h个头
+        k = self.k_linear(k).view(bs, -1, self.h, self.d_k)
+        q = self.q_linear(q).view(bs, -1, self.h, self.d_k)
+        v = self.v_linear(v).view(bs, -1, self.h, self.d_k)
+
+        k = k.transpose(1, 2)
+        q = q.transpose(1, 2)
+        v = v.transpose(1, 2)
+
+        scores = self.attention(q, k, v, self.d_k, mask, self.dropout)
+
+        concat = scores.transpose(1, 2).contiguous().view(bs, -1, self.d_model)
+        output = self.out(concat)
+
+        return output
+
+class FeedForward(nn.Module):
+    def __init__(self, d_model, d_ff=2048, dropout=0.1):
+        super().__init__()
+
+        self.linear_1 = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model)
+
+    def forward(self, x):
+
+        x = self.dropout(F.relu(self.linear_1(x)))
+        x = self.linear_2(x)
+
+        return x
+
