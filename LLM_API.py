1904f921d00c2cb0bf4b955cfd99e44b5b9047d8
wclcatdog <77136368+wclcatdog@users.noreply.github.com>
2025-01-12 11:37:16 +0800

    Update LLM_API.py
    Add a new test case for calling GLM-4

--- LLM_API.py
+++ LLM_API.py
@@ -1,5 +1,8 @@
 import requests
 import json
+import time
+import jwt
+import requests
 
 API_KEY = "你的API Key"
 SECRET_KEY = "你的Secret Key"
@@ -40,6 +43,43 @@ def get_access_token():
     params = {"grant_type": "client_credentials", "client_id": API_KEY, "client_secret": SECRET_KEY}
     return str(requests.post(url, params=params).json().get("access_token"))
 
+def test_GLM4():    
+    # 实际KEY，过期时间
+    def generate_token(apikey: str, exp_seconds: int):
+        try:
+            id, secret = apikey.split(".")
+        except Exception as e:
+            raise Exception("invalid apikey", e)
+    
+        payload = {
+            "api_key": id,
+            "exp": int(round(time.time() * 1000)) + exp_seconds * 1000,
+            "timestamp": int(round(time.time() * 1000)),
+        }
+        return jwt.encode(
+            payload,
+            secret,
+            algorithm="HS256",
+            headers={"alg": "HS256", "sign_type": "SIGN"},
+        )
+    #https://open.bigmodel.cn/api/paas/v4/chat/completions
+    url = "https://open.bigmodel.cn/api/paas/v4/chat/completions"
+    API_KEY = "YOUR API KEY"
+    headers = {
+      'Content-Type': 'application/json',
+      'Authorization': f"Bearer {API_KEY}"
+    }
+    
+    data = {
+        "model": "glm-3-turbo",
+        "messages": [{"role": "user", "content": """你好"""}]
+    }
+    
+    response = requests.post(url, headers=headers, json=data)
+    
+    print("Status Code", response.status_code)
+    #print("JSON Response ", response.json())
+    print(response.json()['choices'][0]['message']['content'])
 
 if __name__ == '__main__':
     main()
